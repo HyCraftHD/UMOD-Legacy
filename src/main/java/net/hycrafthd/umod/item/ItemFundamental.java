9a4261f774c4692032399ad58bed58589c89ce3e
MrTroble <nifroh56@gmail.com>
2015-11-20 22:19:36 +0100

    Revert "Barrel Update"
    This reverts commit 989857bbde63ee76d4ef66df5af4cd25453c5228.

--- src/main/java/net/hycrafthd/umod/item/ItemFundamental.java
+++ src/main/java/net/hycrafthd/umod/item/ItemFundamental.java
@@ -1,10 +1,7 @@
 package net.hycrafthd.umod.item;
 
-import net.hycrafthd.umod.schematic.SchematicInfestedRuin1;
-import net.hycrafthd.umod.utils.GenerationUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public class ItemFundamental extends ItemBase {
@@ -15,8 +12,10 @@ public class ItemFundamental extends ItemBase {
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
-		BlockPos pos = new BlockPos(playerIn.posX, GenerationUtils.getWorldHeightAt(worldIn, playerIn.getPosition().getX(), playerIn.getPosition().getZ()), playerIn.posZ);
-		new SchematicInfestedRuin1().generate(worldIn, pos.getX(), pos.getY(), pos.getZ());
+		// BlockPos pos = new BlockPos(playerIn.posX, playerIn.posY,
+		// playerIn.posZ);
+		// new SchematicInfestedPortal().generate(worldIn, pos.getX(),
+		// pos.getY(), pos.getZ());
 		return super.onItemRightClick(itemStackIn, worldIn, playerIn);
 	}
 }
